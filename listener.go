cd6ec80fdcf40b04f01272331d04d5d604f6709d
konstantinshishkin <k.shishkin@brandquad.ru>
2024-07-08 13:58:56 +0300

    start microservices

--- listener.go
+++ listener.go
@@ -1,30 +1,31 @@
 package dzi
 
-import (
-	"fmt"
-	"github.com/kelseyhightower/envconfig"
-	amqp "github.com/rabbitmq/amqp091-go"
-)
-
-type rabbitSettings struct {
-	Host string `envconfig:"RABBITMQ_HOST" required:"true" default:"localhost"`
-	Port string `envconfig:"RABBITMQ_PORT" required:"true" default:"5672"`
-	User string `envconfig:"RABBITMQ_USER" required:"true" default:"admin"`
-	Pass string `envconfig:"RABBITMQ_PASS" required:"true" default:"admin"`
-}
-
-var RabbitMQConn *amqp.Connection
-
-func init() {
-	var c rabbitSettings
-	OrPanic(envconfig.Process("", &c))
-
-	_c, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s/", c.User, c.Pass, c.Host, c.Port))
-	OrPanic(err)
-
-	RabbitMQConn = _c
-}
-
-func disconnect() {
-	RabbitMQConn.Close()
-}
+//
+//import (
+//	"fmt"
+//	"github.com/kelseyhightower/envconfig"
+//	amqp "github.com/rabbitmq/amqp091-go"
+//)
+//
+//type rabbitSettings struct {
+//	Host string `envconfig:"RABBITMQ_HOST" required:"true" default:"localhost"`
+//	Port string `envconfig:"RABBITMQ_PORT" required:"true" default:"5672"`
+//	User string `envconfig:"RABBITMQ_USER" required:"true" default:"admin"`
+//	Pass string `envconfig:"RABBITMQ_PASS" required:"true" default:"admin"`
+//}
+//
+//var RabbitMQConn *amqp.Connection
+//
+//func init() {
+//	var c rabbitSettings
+//	OrPanic(envconfig.Process("", &c))
+//
+//	_c, err := amqp.Dial(fmt.Sprintf("amqp://%s:%s@%s:%s/", c.User, c.Pass, c.Host, c.Port))
+//	OrPanic(err)
+//
+//	RabbitMQConn = _c
+//}
+//
+//func disconnect() {
+//	RabbitMQConn.Close()
+//}
