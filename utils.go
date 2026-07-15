5df824f174c127ef12a77f9b46b6ffc5a7222491
Sergei Prokhorenko <s.prohorenko@brandquad.ru>
2026-07-15 11:57:50 +0300

    Revert "Adds dUseBigTIFF=true"
    This reverts commit 5179743dda0a6070a9ffd9aacb9fe309a19b8104.

--- utils.go
+++ utils.go
@@ -227,7 +227,6 @@ func callGS(filename, output string, page *pageSize, device string, c *Config) (
 		printSpotCmyk,
 		"-dAlignToPixels=1",
 		"-dGridFitTT=0",
-		"-dUseBigTIFF=true",
 		"-dTextAlphaBits=4",
 		dUsePDFX3,
 		fmt.Sprintf("-dGraphicsAlphaBits=%d", c.GraphicsAlphaBits),
